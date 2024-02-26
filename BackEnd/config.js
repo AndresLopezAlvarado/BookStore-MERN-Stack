6caa8a206f838093c2e8d024192317e5f7c9d325
AndresLopezAlvarado <andreslopezalvarado@hotmail.com>
2024-02-26 18:32:53 -0500

    second commit

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,4 +1,11 @@
-export const PORT = 3000;
-export const MongoDB_URL = "mongodb+srv://andreslopez:andreslopez@cluster0.3ijfq0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+export const PORT = process.env.PORT || 3000;
+
+export const MongoDB_URL =
+  process.env.MongoDB_URL ||
+  "mongodb+srv://andreslopez:andreslopez@cluster0.3ijfq0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // export const MongoDB_URL = "mongodb://localhost:27017/BookStore";
-export const TOKEN_SECRET = "cualquier clave secreta";
+
+export const TOKEN_SECRET =
+  process.env.TOKEN_SECRET || "cualquier clave secreta";
+  
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
